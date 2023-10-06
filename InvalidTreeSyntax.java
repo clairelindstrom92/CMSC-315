ebc2f9780cd5118fe0d409a5bfc9f12077ead525
Claire Lindstrom <90209948+clairelindstrom92@users.noreply.github.com>
2023-10-06 01:00:19 -0400

    Update InvalidTreeSyntax.java

--- InvalidTreeSyntax.java
+++ InvalidTreeSyntax.java
@@ -1,3 +1,8 @@
+/**
+ * This class represents an exception that is thrown when an invalid syntax is detected in a tree.
+ * It extends the Exception class and provides a constructor that takes a message as a parameter.
+ *Claire Lindstrom CMSC 350 Project 3
+ * */
 public class InvalidTreeSyntax extends Exception {
     public InvalidTreeSyntax(String message) {
         super(message);
